3f7071546314d95cd504a1f89a7753d967d9bad3
Gerardo Aguilar <iscgaguilar@gmail.com>
2021-10-25 20:55:10 +0100

    Agregar archivos de proyecto.

---
+++ Contactus/Controllers/ContactMessageController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Contactus.Models;
+
+namespace Contactus.Controllers
+{
+    public class ContactMessageController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContactMessageController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ContactMessage
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ContactMessages.ToListAsync());
+        }
+
+        // GET: ContactMessage/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactMessage = await _context.ContactMessages
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (contactMessage == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactMessage);
+        }
+
+        // GET: ContactMessage/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactMessage/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Username,Message")] ContactMessage contactMessage)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contactMessage);
+                await _context.SaveChangesAsync();
+                //return RedirectToAction(nameof(Details));
+                return RedirectToAction("Details", new { id = contactMessage.ID   });
+            }
+            return View(contactMessage);
+        }
+
+        // GET: ContactMessage/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactMessage = await _context.ContactMessages.FindAsync(id);
+            if (contactMessage == null)
+            {
+                return NotFound();
+            }
+            return View(contactMessage);
+        }
+
+        // POST: ContactMessage/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Username,Message")] ContactMessage contactMessage)
+        {
+            if (id != contactMessage.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contactMessage);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactMessageExists(contactMessage.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contactMessage);
+        }
+
+        // GET: ContactMessage/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contactMessage = await _context.ContactMessages
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (contactMessage == null)
+            {
+                return NotFound();
+            }
+
+            return View(contactMessage);
+        }
+
+        // POST: ContactMessage/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contactMessage = await _context.ContactMessages.FindAsync(id);
+            _context.ContactMessages.Remove(contactMessage);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactMessageExists(int id)
+        {
+            return _context.ContactMessages.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Contactus/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contactus.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContactMessage> ContactMessages { get; set; }
+    }
+}
